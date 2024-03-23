1de156fc0dd9698170d6ed12382dea5f7fb28e79
hertheyhermee <tiamiyuhayzed@gmail.com>
2024-03-23 15:16:50 +0100

    update

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -18,11 +18,35 @@ const ContactUs = () => {
       <div className="contact-us-form">
         <div className="container">
           <div className="block">
-            <div className="row justify-content-center">
-              <div className="col-md-8 col-lg-8 pb-4">
+            <div className="row">
+              <div className="col-12 col-md-6 col-lg-4">
+                <div className="location">
+                  <span>
+                    <FaLocationDot color="white" size={18} className="" />
+                  </span>
+                  <p>43 Raymouth Rd. Baltemoer, London 3910</p>
+                </div>
+              </div>
+              <div className="col-12 col-md-6 col-lg-4 mt-4 mt-md-0 ">
+                <div className="message">
+                  <span>
+                    <IoIosMail color="white" size={18} className="" />
+                  </span>
+                  <p>info@yourdomain.com</p>
+                </div>
+              </div>
+              <div className="col-12 col-md-6 col-lg-4 mt-4 mt-lg-0">
+                <div className="location">
+                  <span>
+                    <BsFillTelephoneFill color="white" size={18} className="" />
+                  </span>
+                  <p>+1 294 3925 3939</p>
+                </div>
+              </div>
+              {/* <div className="col-md-8 col-lg-8 pb-4">
                 <div className="row mb-5">
                   <div className="col-lg-4 mb-4">
-                    <div className="d-flex">
+                    <div className="d-flex align-items-center">
                       <div className="location d-flex justify-content-center align-items-center">
                         <FaLocationDot color={"white"} />
                       </div>
@@ -34,7 +58,7 @@ const ContactUs = () => {
                     </div>
                   </div>
                   <div className="col-lg-4 mb-4">
-                    <div className="d-flex">
+                    <div className="d-flex align-items-center">
                       <div className="location d-flex justify-content-center align-items-center">
                         <IoIosMail color={"white"} />
                       </div>
@@ -44,7 +68,7 @@ const ContactUs = () => {
                     </div>
                   </div>
                   <div className="col-lg-4 mb-4">
-                    <div className="d-flex">
+                    <div className="d-flex align-items-center">
                       <div className="location d-flex justify-content-center align-items-center">
                         <BsFillTelephoneFill color={"white"} />
                       </div>
@@ -61,7 +85,7 @@ const ContactUs = () => {
                         
                     </div>
                 </form>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
